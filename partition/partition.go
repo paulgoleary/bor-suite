19fd4b2011e2976381ed937242bd870d45f3a846
Paul O'Leary <pauloleary@Pauls-MacBook-Pro.local>
2022-04-18 16:43:10 -0700

    WIP - research coding for db partitioning.

--- partition/partition.go
+++ partition/partition.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"github.com/maticnetwork/bor/core/rawdb"
+	"github.com/maticnetwork/bor/ethdb"
+	"os"
+	"path/filepath"
+)
+
 func main() {
-	println("hello world")
+	argsWithoutProg := os.Args[1:]
+
+	dbPath := argsWithoutProg[0]
+
+	if sourceDb, err := rawdb.NewLevelDBDatabaseWithFreezer(dbPath, 0, 0, filepath.Join(dbPath, "ancient"), ""); err != nil {
+		panic(err)
+	} else {
+
+		db0Path := dbPath + ".0"
+		db1Path := dbPath + ".1"
+
+		var part0Db, part1Db ethdb.Database
+
+		if part0Db, err = rawdb.NewLevelDBDatabaseWithFreezer(db0Path, 0, 0, filepath.Join(db0Path, "ancient"), ""); err != nil {
+			panic(err)
+		}
+
+		if part1Db, err = rawdb.NewLevelDBDatabaseWithFreezer(db1Path, 0, 0, filepath.Join(db1Path, "ancient"), ""); err != nil {
+			panic(err)
+		}
+
+		// TODO: batching?
+		
+		iterAll := sourceDb.NewIterator(nil, nil)
+		cntIter := 0
+		for iterAll.Next() {
+			if len(iterAll.Key()) == 0 {
+				panic("should not happen - empty key value")
+			}
+			if iterAll.Key()[0] < 0x80 {
+				if err = part0Db.Put(iterAll.Key(), iterAll.Value()); err != nil {
+					panic(err)
+				}
+			} else {
+				if err = part1Db.Put(iterAll.Key(), iterAll.Value()); err != nil {
+					panic(err)
+				}
+			}
+			cntIter++
+			if cntIter%1000 == 0 {
+				println(fmt.Sprintf("iteration count %v", cntIter))
+			}
+		}
+	}
 }
