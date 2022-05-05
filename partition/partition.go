192c75b533781ae2c5639e67e29ed7717236fe83
Paul O'Leary <pauloleary@Pauls-MacBook-Pro.local>
2022-05-04 19:12:44 -0700

    Factor slice function to be able to run in parallel.

--- partition/partition.go
+++ partition/partition.go
@@ -108,41 +108,45 @@ func POCPartitionDatabase2(dbPath, targetPath string, numParts int, report func(
 
 		wp := workerpool.New(2)
 
-		for i := 0; i < 0x100; i += 0x80 {
-			partPrefix := byte(i)
-			wp.Submit(func() {
-				sliceTarget := part0Db
-				if partPrefix >= 0x80 {
-					sliceTarget = part1Db
-				}
-				iterSlice := sourceDb.NewIterator([]byte{partPrefix}, nil)
-				currBatch := sliceTarget.NewBatch()
-				iterCnt := 0
-				for iterSlice.Next() {
-					if sliceErr := currBatch.Put(iterSlice.Key(), iterSlice.Value()); sliceErr != nil {
+		sliceFunc := func(partPrefix byte, sliceTarget ethdb.Database) {
+			iterSlice := sourceDb.NewIterator([]byte{partPrefix}, nil)
+			currBatch := sliceTarget.NewBatch()
+			iterCnt := 0
+			for iterSlice.Next() {
+				if sliceErr := currBatch.Put(iterSlice.Key(), iterSlice.Value()); sliceErr != nil {
+					panic(sliceErr)
+				} else if currBatch.ValueSize() > 10*ethdb.IdealBatchSize {
+					if sliceErr = currBatch.Write(); sliceErr != nil {
 						panic(sliceErr)
-					} else if currBatch.ValueSize() > 2_000_000 /* ethdb.IdealBatchSize */ {
-						if sliceErr = currBatch.Write(); sliceErr != nil {
-							panic(sliceErr)
-						}
-						currBatch.Reset()
 					}
-					iterCnt++
-					if iterCnt%10000 == 0 {
-						subPrefix := 0
-						if len(iterSlice.Key()) > 1 {
-							subPrefix = int(iterSlice.Key()[1])
-						}
-						report(fmt.Sprintf("slice prefix '%v.%v', iteration count %v", partPrefix, subPrefix, iterCnt))
+					currBatch.Reset()
+				}
+				iterCnt++
+				if iterCnt%10000 == 0 {
+					subPrefix := 0
+					if len(iterSlice.Key()) > 1 {
+						subPrefix = int(iterSlice.Key()[1])
 					}
+					report(fmt.Sprintf("slice prefix '%v.%v', iteration count %v", partPrefix, subPrefix, iterCnt))
 				}
-				// make sure we write out the last batch
-				if err = currBatch.Write(); err != nil {
-					panic(err)
-				}
-				report(fmt.Sprintf("FINISHED slice prefix '%v', iteration count %v", partPrefix, iterCnt))
-			})
+			}
+			// make sure we write out the last batch
+			if err = currBatch.Write(); err != nil {
+				panic(err)
+			}
+			report(fmt.Sprintf("FINISHED slice prefix '%v', iteration count %v", partPrefix, iterCnt))
 		}
+
+		wp.Submit(func() {
+			for i := 0; i < 0x80; i += 1 {
+				sliceFunc(byte(i), part0Db)
+			}
+		})
+		wp.Submit(func() {
+			for i := 0x80; i < 0x100; i += 1 {
+				sliceFunc(byte(i), part1Db)
+			}
+		})
 
 		wp.StopWait()
 	}
